d010d70e785d841e456788a7dffa360a8c007205
Sariframadansyah03 <92282010+Sariframadansyah03@users.noreply.github.com>
2021-12-14 15:06:50 +0700

    Add files via upload

---
+++ soal 2/soal 2.js
@@ -0,0 +1,9 @@
+ function hitung(){
+        const dollar = 14650;
+        let dollar = document.getElementById("dollar").value;
+        console.log ("$ : " + dollar);
+        let hasil = dollar * idr;
+        console.log("rupiah : " + _rupiah);
+        let area_hasil = document.getElementById("hasil");
+        area_hasil.innerHTML = "Rupiah : " + _rupiah
+    }

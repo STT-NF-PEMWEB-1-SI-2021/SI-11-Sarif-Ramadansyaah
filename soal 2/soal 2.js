3af57be81512140264debd3dc3cb5385589596aa
Sariframadansyah03 <92282010+Sariframadansyah03@users.noreply.github.com>
2021-12-14 10:12:12 +0700

    Delete soal 2 directory

--- soal 2/soal 2.js
+++
@@ -1,9 +0,0 @@
- function hitung(){
-        const rupiah = 14650;
-        let dollar = document.getElementById("uang").value;
-        console.log ("$ : " + dollar);
-        let _rupiah = rupiah * dollar;
-        console.log("rupiah : " + _rupiah);
-        let area_hasil = document.getElementById("hasil");
-        area_hasil.innerHTML = "Rupiah : " + _rupiah
-    }

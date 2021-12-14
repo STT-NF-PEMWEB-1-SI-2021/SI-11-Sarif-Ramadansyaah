1a0d032c5b62153536c5255a1d8572be58c9a922
Sariframadansyah03 <92282010+Sariframadansyah03@users.noreply.github.com>
2021-12-14 10:12:46 +0700

    Delete soal 4 directory

--- soal 4/soal-4.js
+++
@@ -1,6 +0,0 @@
-function hitung(){
-    let x = Number(document.getElementById("bil1").value);
-    let y = Number(document.getElementById("bil2").value);
-    let z = x + y;
-    document.getElementById("bil3").value=z;
-}

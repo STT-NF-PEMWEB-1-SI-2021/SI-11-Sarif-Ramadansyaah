3fc6578da38ac845cf1d00ed9b7517680e4719f9
Sariframadansyah03 <92282010+Sariframadansyah03@users.noreply.github.com>
2021-12-14 15:24:31 +0700

    Add files via upload

---
+++ soal 4/soal-4.js
@@ -0,0 +1,6 @@
+function Jumlahan(){
+    Var x = Number(document.getElementById("bil1").value);
+    Var y = Number(document.getElementById("bil2").value);
+    Var z = x + y;
+    document.getElementById("hasil").value=z;
+}
